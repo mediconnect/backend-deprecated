dac93e557904ea505c602e99ce046134f1fd7537
jiashenC <caojiashen24@gmail.com>
2017-03-03 08:56:19 -0500

    modify settings to connect to mysql server

--- medi_connect_site/medi_connect_site/settings.py
+++ medi_connect_site/medi_connect_site/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'medi_connect_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mediconnect',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
