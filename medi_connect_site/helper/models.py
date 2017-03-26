8683114c167e9819a2814ba8f0e552be978e0e7d
jiashenC <caojiashen24@gmail.com>
2017-03-25 23:51:54 -0400

    add helper models

---
+++ medi_connect_site/helper/models.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+from django.db import models
+from customer.models import Customer
+
+
+# Create your models here.
+class Hospital(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    area = models.CharField()
+    capacity = models.IntegerField()
+    website = models.URLField()
+    introduction = models.TextField()
+
+
+class Disease(models.Model):
+    name = models.CharField()
+    category = models.CharField()
+    keyword = models.CharField()
+
+
+class Rank(models.Model):
+    rank = models.IntegerField()
+    hospital = models.OneToOneField(Hospital)
+    disease = models.OneToOneField(Disease)
+
+
+class Order(models.Model):
+    customer = models.OneToOneField(Customer)
+    hospital = models.OneToOneField(Hospital)
+    disease = models.OneToOneField(Disease)
+    # translator = models.OneToOneField(Translator)
+
+
+class Status(models.Model):
+    order = models.OneToOneField(Order)
+    is_paid = models.BinaryField()
+    is_translated = models.BinaryField()
+    is_confirmed = models.BinaryField()
+    is_delivered = models.BinaryField()
+    is_emergency = models.BinaryField()
+    is_completed = models.BinaryField()
