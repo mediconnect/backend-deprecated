38b09fef17871c3f3a8efdea08434975b03450ba
jiashenC <caojiashen24@gmail.com>
2017-05-24 22:03:32 -0400

    finish all dynamic urls

--- medi_connect_site/info/urls.py
+++ medi_connect_site/info/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^profile/(?P<id>\d+)/$', views.profile, name="info_profile"),
+    url(r'^order/(?P<id>\d+)/$', views.order, name="info_order"),
 ]
--- medi_connect_site/info/views.py
+++ medi_connect_site/info/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from customer.models import Customer
+from helper.models import Order
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def profile(request, id):
     customer = Customer.objects.get(id=id)
     return render(request, 'info_profile.html', {
+        'customer': customer,
+    })
+
+
+@login_required
+def order(request, id):
+    customer = Customer.objects.get(id=id)
+    order_list = Order.objects.filter(customer=customer)
+    return render(request, 'info_order.html', {
+        'order_list': order_list,
         'customer': customer,
     })
