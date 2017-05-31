2d439ba5ad718c5fa2361fb9bf9e9955459bd104
gabriel <runye.wang@emory.edu>
2017-05-30 22:09:30 -0400

    translator

--- medi_connect_site/translator/urls.py
+++ medi_connect_site/translator/urls.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import authenticate, login
 
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'},name='translator/login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
+    url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
     url(r'^admin/', admin.site.urls),
 ]
--- medi_connect_site/translator/views.py
+++ medi_connect_site/translator/views.py
@@ -1,7 +1,39 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from translator.models import Translator
+
 
 
 # Create your views here.
-def translator(request):
+@login_required
+def translator(request,user):
+	translator = Translator.objects.get(user = user)
 	return render(request, "translator/main.html",{})
+def home(request):
+    documents = Document.objects.all()
+    return render(request, 'core/home.html', { 'documents': documents })
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'core/simple_upload.html')
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
