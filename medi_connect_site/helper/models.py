4348cc4988e53859589119484c339e42a1f2d0ea
gabriel <runye.wang@emory.edu>
2017-07-28 17:47:25 -0400

    fix datetime

--- medi_connect_site/helper/models.py
+++ medi_connect_site/helper/models.py
@@ -142,7 +142,7 @@ class Order(models.Model):
     disease = models.ForeignKey('Disease', on_delete=models.CASCADE, null=True)
     week_number_at_submit = models.IntegerField(default=0)
     # use week_number_at_submit to hold the week number and calculate the submit deadline
-    submit = models.DateTimeField(default=django.utils.timezone.now)  # datetime of receiving the order
+    submit = models.DateTimeField(default=datetime.datetime.now(utc_8))  # datetime of receiving the order
     # all origin document uploaded by customer
     origin = models.ManyToManyField('Document', related_name='original_file')
     # all feedback document TRANSLATED and APPROVED
