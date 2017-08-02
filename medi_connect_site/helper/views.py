ffada449e763a12cc8052bbaf63e55791a170f86
jiashenC <caojiashen24@gmail.com>
2017-08-02 18:34:01 -0400

    fix hospital order

--- medi_connect_site/helper/views.py
+++ medi_connect_site/helper/views.py
@@ -98,7 +98,7 @@ def hospital(request, hospital_id, disease_id):
         if int(each.status) == 0:
             order = each
             break
-    order = Order(hospital=hosp, status=0, disease=dis, customer=customer) if order is None else order
+    order = Order(hospital=hosp, step=-1, status=0, disease=dis, customer=customer) if order is None else order
     order.save()
     if order.step == 0:
         return redirect('order_info_first', order_id=int(order.id), slot_num=order.week_number_at_submit)
