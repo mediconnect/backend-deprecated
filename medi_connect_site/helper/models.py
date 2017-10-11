62f9977869271af90a66109d72400e70783525b3
unknown <runye.wang@emory.edu>
2017-10-11 09:34:03 -0400

    fix slots

--- medi_connect_site/helper/models.py
+++ medi_connect_site/helper/models.py
@@ -94,6 +94,21 @@ class Slot(models.Model):
     class Meta:
         db_table = 'slot'
 
+    def reset_slot(self):
+        self.slots_open_0 = self.slots_open_1
+        self.slots_open_1 = self.slots_open_2
+        self.slots_open_2 = self.slots_open_3
+        self.slots_open_3 = self.default_slots
+        self.save()
+
+    def set_slots(self, slot_dic={}):
+        self.default_slots = slot_dic[0]
+        self.slots_open_0 = slot_dic[1]
+        self.slots_open_1 = slot_dic[2]
+        self.slots_open_2 = slot_dic[3]
+        self.slots_open_3 = slot_dic[4]
+        self.save()
+
 
 class Price(models.Model):
     hospital = models.ForeignKey(Hospital, unique=False, default=None, related_name='hospital_price')
