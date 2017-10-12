2d24a812f2127a57bb884a19c121c509acc9ddf9
unknown <runye.wang@emory.edu>
2017-10-12 09:32:35 -0400

    password check

--- medi_connect_site/supervisor/urls.py
+++ medi_connect_site/supervisor/urls.py
@@ -5,12 +5,17 @@ from core import views as core_views
 
 urlpatterns = [
     url(r'^login/$', core_views.auth, name='supervisor_login'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/supervisor/login'}, name='supervisor_logout'),
+
+
+
     url(r'^home/(?P<id>\d+)/$',supervisor_views.supervisor, name = 'supervisor_home'),
     url(r'^orders/(?P<id>\d+)/(?P<status>\w+)/$',supervisor_views.order_status, name = 'order_status'),
     url(r'^ajax/update_result/$', supervisor_views.update_result, name='update_result'),
     url(r'^ajax/validate_pwd/$', supervisor_views.validate_pwd, name='validate_pwd'),
-    url(r'^logout/', auth_views.logout, {'next_page': '/supervisor/login'}, name='supervisor_logout'),
+
     url(r'^trans_signup/(?P<id>\d+)/$',supervisor_views.trans_signup ,name = 'trans_signup'),
+    url(r'^send_password/(?P<id>\d+)/(?P<user_id>\d+)/$', auth_views.password_reset, {'template_name': 'reset_password.html'}, name='send_password'),
 
     url(r'^detail/(?P<id>\d+)/(?P<order_id>\d+)/$',supervisor_views.detail ,name = 'detail'),
     url(r'^assign/(?P<id>\d+)/(?P<order_id>\d+)/$',supervisor_views.assign ,name = 'assign'),
