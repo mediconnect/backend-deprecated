f8ad96591bde6bad339d7a299d76921f1efc159d
Jiashen Cao <caojiashen24@gmail.com>
2018-01-10 10:34:53 -0500

    fix documents complete check

--- medi_connect_site/info/views.py
+++ medi_connect_site/info/views.py
@@ -230,6 +230,7 @@ def add_doc(request, order_id):
         if all(Document.objects.filter(description=document, order=order, type=0).count() > 0 for document in
                required + optional):
             order.document_complete = True
+            order.save()
         return redirect('info_order_detail', order.id)
     return render(request, 'add_doc.html', {
         'form': create_form(int(order.hospital.id), int(order.disease.id), DocAddForm(), only_optional=True),
